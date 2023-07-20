3f604157f0af97f0ace8ba17188ce351dc1cd422
Nelson Lamounier <lamounierleao@gmail.com>
2023-07-20 11:48:47 +0000

    Use MongoDB with Python

---
+++ connect_db.py
@@ -0,0 +1,15 @@
+import pymongo
+
+DB_NAME = "contracts"
+
+def get_database_connection():
+    """
+    this function should create a connection with MongoClient and return a
+    dabatase object
+    """
+
+    from pymongo import MongoClient
+    client = MongoClient('mongodb://localhost:27017/')
+
+    return client[DB_NAME]
+
